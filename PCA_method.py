dbe23079024c0c5aead31cbcb5cd7aa760004f27
Nicholas Gordon <73216712+ngordon0110@users.noreply.github.com>
2024-11-29 23:03:59 -0500

    Add files via upload

---
+++ PCA_method.py
@@ -0,0 +1,158 @@
+"""
+Copyright 2024 Nicholas Gordon
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of 
+this software and associated documentation files (the “Software”), to deal in the 
+Software without restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import sys
+import os
+import spectral
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from tqdm import tqdm
+
+"""
+Big O notation for principle component analysis is O(p^2 n + p^3) for n sized input with p variables
+Covariance matrix calculation is O(p^2 n)
+Eigenvalue decomposition is O(p^3)
+"""
+
+def COMPUTE_PCA(hyperspectral_image, n_comp):
+    """
+    PURPOSE:
+        This function performs principle component analysis on a hyperspectral image
+        The components are calculated compared to each pixel spectra
+    EXPECTED INPUT:
+        Two inputs are expected, one of which requires specific formatting
+        hyperspectral_image should be 3 dimensional and formated as such (bands, x, y) with bands being the spectra, x and y being the coordinates in the image
+        n_comp is an integer indicating how many components should be calculated
+    OUTPUT:
+        Returns a hyperspectral image with the 0th dimension being each principal component, the x and y values are then the calculated principle components 
+
+    EXAMPLE:
+    The hyperspectral image of interest is 200 bands with dimensions 500 by 500
+    Formated as (band, x, y) with name hyperspectal_in
+    The number of components to calculate in this instance are 10 but can be decided using the function below (COMPUTE_COMPONENTS)
+    
+    pca_image = COMPUTE_PCA(hyperspectral_in, 10)
+
+    The resultant pca_image will have dimensionality (10,500,500)
+    Each 0th dimenson represents a principle component.
+    """
+
+    # Reshape the image from (n, x, y) to (n, x*y)
+    layers, height, width = hyperspectral_image.shape
+    reshaped_data = hyperspectral_image.reshape(layers, height * width)
+    
+    std_data = StandardScaler().fit_transform(reshaped_data)
+    
+    # Perform PCA on the pixel spectra, in this case because it's an ENVI file that's dimension 0
+    pca = PCA(n_components=n_comp)
+    p_components = pca.fit_transform(std_data.T)  # Transpose back for PCA... remember that pesky dimenson zero???
+    
+    # Reshape the principal components back to (components, height, width)
+    pca_image = p_components.T.reshape(n_comp, height, width)
+    
+    return pca_image
+
+
+def COMPUTE_COMPONENTS(hyperspectral_image, v_percent):
+    """
+    PURPOSE:
+        This function is used to find the number of components needed to describe a set percentage of variance in the original nth dimensional matrix
+    EXPECTED INPUT:
+        Two inputs, hyperspectral_image and v_percent
+        hyperspectral_image should be 3 dimensional and formated as such (bands, x, y) with bands being the spectra, x and y being the coordinates in the image
+        v_percent is the variance percentage you want explained by the components, expected input is a float with 2 digit precision
+    OUTPUT:
+        Returns an integer value that coincides with number of components necessary to describe the requested percentage of variance
+
+    EXAMPLE:
+    The hyperspectral image of interest is 200 bands with dimensions 500 by 500
+    Formated as (band, x, y) with name hyperspectal_in
+    We want to know how many bands are needed to explain 80% of the variance in our original image
+
+    num_components = COMPUTE_COMPONENTS(hyperspectral_in, 0.80)
+
+    The num_components variable will then be an integer value corresponding to number of components
+    """
+
+    # Reshape the image from (layer, x, y) to (layer, x*y) we want to compute the principal components of each pixel spectra
+    layers, height, width = hyperspectral_image.shape
+    reshaped_data = hyperspectral_image.reshape(layers, height * width)
+
+    #standardize the dataset
+    standardized_data = StandardScaler().fit_transform(reshaped_data)
+    
+    pca = PCA(n_components=None)
+    pca_fit = pca.fit_transform(standardized_data.T)
+
+    #find the number of components that satisfies v percentage of variance, specified at call
+    n_components = 1 + np.argmax(np.cumsum(pca.explained_variance_ratio_) >= v_percent)
+
+
+    return n_components
+
+
+"""
+First define the file path to the header (HDR) file, it is CRITICAL that the image (IMG) file is in the SAME DIRECTORY WITH THE SAME NAME!!!!
+
+Then read the hyperspectral image to memory
+We start with saying we want to move the axis around by transposing our input file and then swapping the last portions of array (np.moveaxis)
+This transformation makes working the the data later more intuitive
+Format of the original input is (y,x,bands) which is not impossible but weird to work with
+After transformation we're left with (bands,x,y)
+The spectral.open_image is taking the path to the header file we defined earlier and then loading the accompanying img file
+This is why it is CRITICAL to have the .img file in the SAME directory with the SAME name!!!!
+"""
+
+img_input_path = sys.argv[1]
+if os.path.exists(img_input_path) == False:
+    raise Exception("Image file doesn't exist!! Please try again with a real file...")
+
+img_save_path = sys.argv[2]
+
+print("Loading image into memory")
+hyperspectral_img_in = np.transpose(spectral.open_image(img_input_path).load(), (2, 0, 1))
+
+print("Calculating PCA")
+#call COMPUTE_COMPONENTS for input to PCA computation
+num_components = COMPUTE_COMPONENTS(hyperspectral_img_in, 0.90)
+#call PCA_RESULTS to create our hyperspectral PCA image
+pca_result = COMPUTE_PCA(hyperspectral_img_in, num_components)
+
+for i in tqdm(range(pca_result.shape[0]), desc="Saving components to file"):
+    plt.imsave(f'comp{i}_PCA90.tiff', pca_result[i,:,:], cmap='Greys_r')
+
+#transpose the data back to the expected ENVI format for saving
+pca_result = np.transpose(pca_result, (1,2,0))
+
+#define the output path for the file we're going to save, note this includes the file name
+hdr_metadata = {
+    'lines' : pca_result.shape[0], #x
+    'samples' : pca_result.shape[1], #y
+    'bands' : pca_result.shape[2],
+    'interleave' : 'bsq',
+    'data_type' : 4, #32bit float
+    'byte_order' : 0 #small endian
+}
+
+#finally save the file as ENVI img and hdr format, note the funciton only calls for the hdr file name, the img will be auto generated
+spectral.envi.save_image(img_save_path, pca_result, metadata=hdr_metadata, interleave='bsq', force=True)
---
+++ cosine_similarity_method.py
@@ -0,0 +1,131 @@
+"""
+Copyright 2024 Nicholas Gordon
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of 
+this software and associated documentation files (the “Software”), to deal in the 
+Software without restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import spectral
+import pandas as pd
+from tqdm import tqdm
+
+
+def LIB_SEARCH(img_in, library_in, match_thres=0.6, save_thres=.02, legend=False):
+    """
+    PURPOSE:
+        This function will search through input img_in and compare the spectra at each pixel to the USGS specular IR library.
+
+    EXPECTED INPUT:
+        ***it's expected that the input img_in and library_in have been normalized using MAX_NORM, derivative and magnitude will be calculated at call***
+        there are a total of five inputs expected, two of which expect specific formatting
+        ! - img_in should be 3 dimensional and formated as such (bands, x, y) with bands being the spectra, x and y being the coordinates in the image
+        ! - library_in should be a pandas dataframe with keys associated to each spectra and the key representing the mineral ID
+        match_thresh is a float value used as a cutoff for cosine similarity matching between the img_in pixel spectr and USGS library
+        save_thres is a float value used to determine the cutoff for saving similarity map. The percentage is how many pixels of x mineral
+            are present in the original image. I.E. if there are 1000 pixels in the image and 20 are a match for calcite then a calcite similarity
+            map will be saved.
+        finally legend is a boolean value T/F used to determine if the exported image will have a legend including title and color bar.
+
+        ! = required ipnut
+        
+    OUTPUT:
+        In it's current form this function has no particular output but instead will just save the similarity map.
+    """
+
+    #vectorization to calculate derivative and magnitude along 0th axis of n-dimensional array
+    #the magnitude calculation was originally done with np.linalg.norm but is being cranky as of 11-27-24
+    #instead magnitude is calculated manually with the numpy summation and square root function
+    img_in = np.gradient(hyperspectral_img_in, axis=0)
+    img_in_mag = np.sqrt(np.sum(img_in**2, axis=0))
+
+    #Once again utilize vectorization but this time we want to retain the dataframe keys
+    #To do this we spawn sub routines or sub functions with lambda to cast the derivative and magnitude calculations while utilizing the .apply() functionality thereby retaining original dataframe keys
+    library = library_in.apply(lambda col: np.gradient(col.values), axis=0)
+    library_mag = library.apply(lambda col: np.linalg.norm(col.values), axis=0)
+
+    #with the data preprocessed we can start to search the library for mineral ID matches
+    #loop through the library but skip the first column, in this case the USGS libraries first column is just wavenumbers
+    for minName in tqdm(library.columns[1:], desc="Searching library"):
+        #for each mineral create a cosine similarity map, this does so by using tensordot to calculate the dot product
+        #between the current mineral in the library and the input image
+        #we use tensordot from numpy because it allows for matrix calculations along specified axes of input arrays
+        #because our arrays ***SHOULD*** have the same first axis dimensions we can use vectorization here to speed up the process
+        #once we have a dot product we can divide that by the product of the arrays magnitudes using, you guessed it, vectorization
+        #to speed up that process
+        similarity_map = np.tensordot(library[minName], img_in, axes=([0], [0])) / (img_in_mag * library_mag[minName])
+        #once a similarity map exists check to see if any of the pixels correspond to a set match threshold
+        if np.any(similarity_map > match_thres):
+            #if true then create a mask of the dataset, this creates a 2d array of booleans
+            mask = similarity_map > 0.6
+            #.sum() acts kind of similar to if statement default logic, so all true booleans are summed effectively
+            #creating a number of pixels that match the threshold.
+            count = mask.sum()
+            #print(f"{minName}:{count}")
+            #this checks to see what percent of the image is explained by the matched spectra, threshold set at call
+            if count / (img_in.shape[1] * img_in.shape[2]) > save_thres:
+                #check if legend is wanted
+                if legend:
+                    plt.imshow(similarity_map, cmap='turbo', vmin=0, vmax=1)
+                    plt.colorbar(label='Similarity to USGS library')
+                    plt.title(f"Cosine similarity for {minName}")
+                    plt.axis('off')
+                    plt.savefig(f'{minName}_similarity.tiff', format='tiff', dpi=400, bbox_inches='tight')
+                    plt.close()
+                else:
+                    plt.imsave(f'{minName}_similarity.tiff', similarity_map, cmap='turbo', vmin=0, vmax=1)
+
+
+
+def MAX_NORM(array_in):
+    """
+    PURPOSE:
+        Normalize input array to the maximum value of the input expressed as a percentage
+
+    EXPECTED INPUT:
+        The only input is an array, this can be any n>0 dimensional array
+        
+    OUTPUT:
+        returns input array with values along 0th axis normalized to max expressed as a percentage
+    """
+    
+    #utilize vectorization instead of looping through to find max value in first position of array
+    max_value = np.max(array_in, axis=0)
+    #once again we just cast the arrays to normalize, very neat, thanks python
+    out = array_in / max_value
+    return out
+
+img_input_path = sys.argv[1]
+if os.path.exists(img_input_path) == False:
+    raise Exception("Image file doesn't exist!! Please try again with a real file...")
+
+library_input_path = sys.argv[2]
+if os.path.exists(library_input_path) == False:
+    raise Exception("Library file doesn't exist!! Please try again with a real file...")
+
+#load the datasets to memory while doing some preprocessing
+print("Loading image and library into memory")
+#transpose the hyperspectral image to match the expected input format
+hyperspectral_img_in = MAX_NORM(np.transpose(spectral.open_image(img_input_path).load(), (2,0,1)))
+#load library with pandas as a dataframe, keys correspond to csv file headers
+USGS_in = MAX_NORM(pd.read_csv(library_input_path))
+
+print("Processing data")
+LIB_SEARCH(hyperspectral_img_in, USGS_in, match_thres=float(sys.argv[3]), save_thres=float(sys.argv[4]), legend=bool(sys.argv[5]))
